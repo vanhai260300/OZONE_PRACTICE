226164f579e32be0a9704cc3160dd4cefdc0d0f5
Firstname Lastnamevanhai260300 <your_email@youremail.com>
2021-01-22 20:13:35 +0700

    first commit

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+function closeNav(){
+    document.getElementById("sidepanel").style.width = "0";
+}
+function openNav(){
+    document.getElementById("sidepanel").style.width = "25%";
+    console.log("helo");
+}
